71d18383c14ca3603ea9a516ecec773004957676
sungju <lsj0147@naver.com>
2017-12-05 14:39:15 +0900

    리엑트 처음

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';    // var React = require('react');
+
+class App extends React.Component {
+	render () {
+		return (
+			<h1>Hello React</h1>
+		);
+	}
+}
+
+export default App;   // module.export = App;
